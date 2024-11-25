12e3149a6a6eaa6dc4d65daee8370bf27b2c34b9
Nilesh Awachat <nileshawachat11@gmail.com>
2024-11-25 12:16:07 +0530

    first commit

---
+++ src/BgcolorChanger.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("olive")
+  const [colors, setColors] = useState(["red", "black", "yellow", "orange"])
+
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          {colors.map((color, index) => (
+            <button
+              key={index}
+              className="outline-none-px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: color }} 
+              onClick={() => { setColor(color) }}
+            >
+              {color.charAt(0).toUpperCase() + color.slice(1)}
+            </button>
+          ))}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import BgcolorChanger from './BgcolorChanger'
+// import PasswordGenerator from './Passwordgenerator'
+// import App from './MiniContext/App'
+import App from './Themeswitcher/App'
+
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <BgcolorChanger /> */}
+    {/* <PasswordGenerator /> */}
+    {/* <App/> */}
+    <App />
+   
+  </StrictMode>,
+)
